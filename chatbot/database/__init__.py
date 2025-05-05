5b0f43a8c0850dca426967d075c817880b2dd08e
Minh Duong <minhduong@Minhs-MacBook-Pro.local>
2025-05-05 13:02:35 +1000

    Delete unuse files, ignore to secrets

--- chatbot/database/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Database package for handling all database operations.
-This includes vector store operations, data ingestion, and retrieval.
-"""
-
-from .vector_store import AstraVectorStore
-from .ingest import DataIngestor
-
-__all__ = ['AstraVectorStore', 'DataIngestor'] 
